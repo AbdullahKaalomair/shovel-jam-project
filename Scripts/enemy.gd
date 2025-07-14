600591520349f06fa61aa8d464eab2ee1af3fa8f
Abdullah Alomair <88461660+AbdullahKaalomair@users.noreply.github.com>
2025-07-14 17:17:27 +0300

    simple navigation system

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,13 +1,34 @@
 extends CharacterBody2D
 class_name Enemy
 
+@export var target: Node2D
+@onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
+
 var health = 3
 var damage = 1
 
+const SPEED = 130.0
+
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	# These values need to be adjusted for the actor's speed
+	# and the navigation layout.
+	navigation_agent_2d.path_desired_distance = 4.0
+	navigation_agent_2d.target_desired_distance = 4.0
+
+	# Make sure to not await during _ready.
+	actor_setup.call_deferred()
+
+func actor_setup():
+	# Wait for the first physics frame so the NavigationServer can sync.
+	await get_tree().physics_frame
+
+	# Now that the navigation map is no longer empty, set the movement target.
+	set_movement_target(target.position)
+
+func set_movement_target(movement_target: Vector2):
+	navigation_agent_2d.target_position = movement_target
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,10 +37,21 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	
+
+		
+	# direction calculation
+	if navigation_agent_2d.is_navigation_finished():
+		return
+
+	var current_agent_position: Vector2 = global_position
+	var next_path_position: Vector2 = navigation_agent_2d.get_next_path_position()
+
+	velocity = current_agent_position.direction_to(next_path_position) * SPEED 
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
+		
 	move_and_slide()
 
 
