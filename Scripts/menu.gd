ad0de817697305559c0906a15753eed934945cfd
Claude Ni <Claudenij2@gmail.com>
2025-07-17 13:50:34 +0300

    Updated main menu to include a start section and music

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,17 +1,40 @@
 extends Control
 
-var game = "res://Scenes/game.tscn"
+var game_scene = "res://Scenes/game.tscn"
+var tutorial_scene = ""
+var options_scene = ""
+
+var is_game_started = false
+
+@onready var menu_container = $MenuMarginContainer
+@onready var start_container = $StartMarginContainer
+@onready var audio_player = $AudioStreamPlayer
+
+var menu_music = preload("res://Assets/Sounds/Menu/Christmas synths.ogg")
+
+func _ready() -> void:
+	is_game_started = false
+	menu_container.visible = false
+	start_container.visible = true
+
+func _input(event: InputEvent) -> void:
+	if not is_game_started and event.is_pressed():
+		is_game_started = true
+		menu_container.visible = true
+		start_container.visible = false
+		audio_player.stream = menu_music
+		audio_player.play()
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file(game)
+	get_tree().change_scene_to_file(game_scene)
 
 
 func _on_tutorial_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file(tutorial_scene)
 
 
 func _on_options_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file(options_scene)
 
 
 func _on_quit_pressed() -> void:
